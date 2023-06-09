42496d605dad38e5aa6c1c1f6d5c03973aa540db
Tejas-67 <tejasjha3210@gmail.com>
2023-06-09 16:31:10 +0530

    Improved HomeFragment UI

--- app/src/main/java/com/example/repospect/Adapters/SavedRepoAdapter.kt
+++ app/src/main/java/com/example/repospect/Adapters/SavedRepoAdapter.kt
@@ -13,6 +13,9 @@ class SavedRepoAdapter: RecyclerView.Adapter<SavedRepoAdapter.SavedRepoViewHolde
     private var list: List<Repo> = listOf()
     class SavedRepoViewHolder(val view: View): RecyclerView.ViewHolder(view){
         val nameTv: TextView = view.findViewById(R.id.repo_name_tv)
+        val languageTv: TextView = view.findViewById(R.id.languge_tv)
+        val createdAt: TextView = view.findViewById(R.id.created_at_tv)
+        val desc: TextView = view.findViewById(R.id.desc)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SavedRepoViewHolder {
@@ -25,6 +28,17 @@ class SavedRepoAdapter: RecyclerView.Adapter<SavedRepoAdapter.SavedRepoViewHolde
 
     override fun onBindViewHolder(holder: SavedRepoViewHolder, position: Int) {
         holder.nameTv.text=list[position].full_name
+        holder.languageTv.text=list[position].language
+        holder.createdAt.text="Updated on ${list[position].updated_at!!.split('T')[0]}"
+
+        if(list[position].description.isNullOrEmpty()){
+            holder.desc.visibility=View.GONE
+        }
+        else{
+            holder.desc.visibility=View.VISIBLE
+            holder.desc.text=list[position].description
+        }
+
     }
 
     fun updateRcv(l: List<Repo>){
