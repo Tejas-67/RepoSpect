7dea21f0ba6f3b1e228247ab30116373f5931ba8
Tejas-67 <tejasjha3210@gmail.com>
2023-12-13 14:13:22 +0530

    setting up UI for login/signup

---
+++ app/src/main/java/com/example/repospect/Fragments/LoginFragment.kt
@@ -0,0 +1,28 @@
+package com.example.repospect.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.repospect.R
+import com.example.repospect.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {}
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
